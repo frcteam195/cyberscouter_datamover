1d23c04228a0a3876e6dfb6e769d120efdec3549
bontempi <chrisbontempi@compuserve.com>
2022-02-12 15:26:31 -0500

    Initial Android Studio project commit.

---
+++ app/src/main/java/com/frcteam195/cyberscouter_datamover/MainActivity.java
@@ -0,0 +1,14 @@
+package com.frcteam195.cyberscouter_datamover;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
